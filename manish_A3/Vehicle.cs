5276c951f56bc378c12a824d3681910c8d9c2481
manish koladiya <manishkoladiya546@gmail.com>
2024-03-27 21:54:09 -0400

    Car.cs file add

---
+++ manish_A3/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manish_A3
+{
+    internal class Vehicle
+    {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Year { get; set; }
+        public decimal RentalPrice { get; set; }
+
+        public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Year = year;
+            RentalPrice = rentalPrice;
+        }
+
+        public virtual void DisplayDetails()
+        {
+
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Manufacturer: {Manufacturer}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Rental Price $: {RentalPrice}");
+
+        }
+    }
+}
