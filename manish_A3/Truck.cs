27e1bb3834bbe7418fac6695d87eaa9c191971a9
manish koladiya <manishkoladiya546@gmail.com>
2024-03-27 21:57:32 -0400

    Truck.cs file added

---
+++ manish_A3/Truck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manish_A3
+{
+    internal class Truck : Vehicle
+    {
+        public double Capacity { get; set; }
+        public string TruckType { get; set; }
+        public bool FourWheelDrive { get; set; }
+
+        public Truck(string model, string manufacturer, int year, decimal rentalPrice, double capacity, string truckType, bool fourWheelDrive)
+            : base(model, manufacturer, year, rentalPrice)
+        {
+            Capacity = capacity;
+            TruckType = truckType;
+            FourWheelDrive = fourWheelDrive;
+        }
+
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Capacity: {Capacity}");
+            Console.WriteLine($"Truck Type: {TruckType}");
+            Console.WriteLine($"Four Wheel Drive: {FourWheelDrive} \n");
+
+        }
+    }
+}
