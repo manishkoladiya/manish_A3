af940f487641335fe3ac923fd894a1ce4e622714
manish koladiya <manishkoladiya546@gmail.com>
2024-03-27 22:01:10 -0400

    add file RentalAgency.cs

---
+++ manish_A3/RentalAgency.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manish_A3
+{
+    internal class RentalAgency
+    {
+        private List<Vehicle> Fleet = new List<Vehicle>();
+        public decimal TotalRevenue { get; private set; }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            Fleet.Add(vehicle);
+        }
+
+        public void RemoveVehicle(Vehicle vehicle)
+        {
+            Fleet.Remove(vehicle);
+        }
+
+        public void RentVehicle(Vehicle vehicle, int days)
+        {
+            TotalRevenue += vehicle.RentalPrice * days;
+        }
+
+        public void DisplayFleetDetails()
+        {
+            foreach (var vehicle in Fleet)
+            {
+                vehicle.DisplayDetails();
+            }
+        }
+    }
+}
